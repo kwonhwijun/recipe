c5a978d0684e3d79f7e1edf14eca720c999a8552
hwijunkwon <hj.kwon967@gmail.com>
2023-11-22 13:40:45 +0900

    upload

--- models/embedding.py
+++ models/embedding.py
@@ -1,1 +1,18 @@
-print("HI")
+from fractions import Fraction
+from oracle import oracleTopd
+
+def convert_fraction_to_float(quantity):
+    try:
+        return float(Fraction(quantity))
+    except ValueError:
+        return None 
+    
+def convert_unit_to_number(unit):
+    unit_conversion = {
+        'g': 1,
+        '개': 100,
+        '조금' :10
+    }
+    return unit_conversion.get(unit, 1)
+
+
